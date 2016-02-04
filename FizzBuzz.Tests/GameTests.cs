fff6aa8a622e81f734480605a0ed66ff018f9ce3
Brent Sullivan <brntsllvn@gmail.com>
2016-02-04 13:29:12 -0800

    tdd

--- FizzBuzz.Tests/GameTests.cs
+++ FizzBuzz.Tests/GameTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +12,75 @@ namespace FizzBuzz.Tests
     [TestFixture]
     public class GameTests
     {
-        [Test]
-        public void FizzBuzzGame()
+        private Game g;
+
+        [SetUp]
+        public void Init()
         {
-            
+            g = new Game();
+        }
+
+        [Test]
+        public void One()
+        {
+            String fizzbuzz = g.FizzBuzzifier(1);
+            Assert.That(fizzbuzz, Is.EqualTo("1"));
+        }
+
+        [Test]
+        public void Two()
+        {
+            String fizzbuzz = g.FizzBuzzifier(2);
+            Assert.That(fizzbuzz, Is.EqualTo("2"));
+        }
+
+        [Test]
+        public void Three()
+        {
+            String fizzbuzz = g.FizzBuzzifier(3);
+            Assert.That(fizzbuzz, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void Four()
+        {
+            String fizzbuzz = g.FizzBuzzifier(4);
+            Assert.That(fizzbuzz, Is.EqualTo("4"));
+        }
+
+        [Test]
+        public void Five()
+        {
+            String fizzbuzz = g.FizzBuzzifier(5);
+            Assert.That(fizzbuzz, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void Fifteen()
+        {
+            String fizzbuzz = g.FizzBuzzifier(15);
+            Assert.That(fizzbuzz, Is.EqualTo("Fizz Buzz"));
+        }
+
+        [Test]
+        public void OneHundredThree()
+        {
+            String fizzbuzz = g.FizzBuzzifier(103);
+            Assert.That(fizzbuzz, Is.EqualTo("103"));
+        }
+
+        [Test]
+        public void SevenNinetyFive()
+        {
+            String fizzbuzz = g.FizzBuzzifier(795);
+            Assert.That(fizzbuzz, Is.EqualTo("Fizz Buzz"));
+        }
+
+        [Test]
+        public void LargeNum()
+        {
+            String fizzbuzz = g.FizzBuzzifier(10000003);
+            Assert.That(fizzbuzz, Is.EqualTo("10000003"));
         }
     }
 }
--- FizzBuzz/Game.cs
+++ FizzBuzz/Game.cs
@@ -8,5 +8,41 @@ namespace FizzBuzz
 {
     public class Game
     {
+        public string FizzBuzzifier(int fizzBuzzRoundNumber)
+        {
+            String Fizz = "Fizz";
+            String Buzz = "Buzz";
+            String fizzBuzzResult;
+
+            if (DividesByThree(fizzBuzzRoundNumber))
+            {
+                fizzBuzzResult = Fizz;
+
+                if (DividesByFive(fizzBuzzRoundNumber))
+                {
+                    fizzBuzzResult += " " + Buzz;
+                }
+            }
+            else if (DividesByFive(fizzBuzzRoundNumber))
+            {
+                fizzBuzzResult = Buzz;
+            }
+            else
+            {
+                fizzBuzzResult = fizzBuzzRoundNumber.ToString();
+            }
+
+            return fizzBuzzResult;
+        }
+
+        public bool DividesByThree(int i)
+        {
+            return (i % 3 == 0);
+        }
+
+        public bool DividesByFive(int i)
+        {
+            return (i % 5 == 0);
+        }
     }
 }
