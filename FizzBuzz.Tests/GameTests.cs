7d8ef17f86845d1f46229fb5645d7913416a85d0
Brent Sullivan <brntsllvn@gmail.com>
2016-02-04 12:06:25 -0800

    init

---
+++ FizzBuzz.Tests/GameTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzz.Tests
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void FizzBuzzGame()
+        {
+            
+        }
+    }
+}
